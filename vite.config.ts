92ae22a9c004450f292a44178443e55b29d4dc7c
Vitor Franco <vitoroliveirafranco@gmail.com>
2025-03-18 19:22:43 -0300

    add css modules setup add type definition files add package configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,33 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import { resolve } from 'node:path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { resolve } from "node:path";
+import dts from "vite-plugin-dts";
+import { libInjectCss } from 'vite-plugin-lib-inject-css'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    lib: {
-      entry: resolve(__dirname, 'src/components'),
-      name: 'DesignSystem',
-      fileName: 'ds' // output
-    },
-    rollupOptions: {
-      external: ['react', 'react-dom', 'react/jsx-runtime'],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': "ReactDOM"
-        }
-      }
-    },
-  },
+	plugins: [
+		react(),
+    libInjectCss(),
+		dts({
+			include: ["src/components"],
+		}),
+	],
+	build: {
+    copyPublicDir: false,
+		lib: {
+			entry: resolve(__dirname, "src/components"),
+			name: "DesignSystem",
+			fileName: "ds", // output
+		},
+		rollupOptions: {
+			external: ["react", "react-dom", "react/jsx-runtime"],
+			output: {
+				globals: {
+					react: "React",
+					"react-dom": "ReactDOM",
+				},
+			},
+		},
+	},
 });
